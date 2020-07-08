4adfd945fa6cd61ea73ebd52aa8fe24997f610c4
jimmy5678998 <67999796+jimmy5678998@users.noreply.github.com>
2020-07-08 15:59:53 +0800

    Add files via upload

---
+++ ch16 extra.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+script, filename = argv
+a = True
+while a:
+    print(f"We're going to read {filename}")
+    print("Type \"Yes\" if you want to read.")
+    print("Type \"No\" if you don't want.")
+    choice = input("> ")
+
+    if choice == "No":
+        a = False
+        break
+    if choice != "No" or "Yes":
+        print("Please input the correct word to indicate your preference.")
+    if choice == "Yes":
+        print("Opening the file...")
+        txt = open(filename)
+
+
+
+
