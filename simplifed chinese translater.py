bcff17592f0b966532277a3cd66d5b3fa913ac82
jimmy5678998 <67999796+jimmy5678998@users.noreply.github.com>
2020-07-15 01:52:23 +0800

    Add files via upload

---
+++ simplifed chinese translater.py
@@ -0,0 +1,24 @@
+import sys
+script, input_encoding, error, topic = sys.argv
+
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(cooked_string)
+
+
+languages = open(topic, encoding="GB2312", errors="replace")
+
+
+main(languages, input_encoding, error)
